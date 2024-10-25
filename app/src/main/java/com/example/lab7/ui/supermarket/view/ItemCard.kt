1d9f1ad6b15c723e819487f5148681ab374348f0
AngelEsquit <esq23221@uvg.edu.gt>
2024-10-25 13:04:06 -0600

    1

--- app/src/main/java/com/example/lab7/ui/supermarket/view/ItemCard.kt
+++ app/src/main/java/com/example/lab7/ui/supermarket/view/ItemCard.kt
@@ -1,55 +1,33 @@
 package com.example.lab7.ui.supermarket.view
 
 import android.annotation.SuppressLint
-import android.content.pm.PackageManager
-import android.media.Image
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.lifecycle.ViewModelProvider
-import coil.compose.AsyncImagePainter
-import coil.compose.ImagePainter
 import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
 import com.example.lab7.MyApp
 import com.example.lab7.database.supermarket.SupermarketItemEntity
-import com.example.lab7.navigation.NavigationState
-import com.example.lab7.navigation.navigateTo
-import com.example.lab7.ui.supermarket.repositories.SupermarketRepository
 import com.example.lab7.ui.supermarket.viewmodel.SupermarketViewModel
 import com.example.lab7.ui.supermarket.viewmodel.SupermarketViewModelFactory
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ShowImage(name: String, quantity: String, path: String, viewModel: SupermarketViewModel) {
+fun ItemCard(name: String, quantity: String, path: String, viewModel: SupermarketViewModel) {
     // /storage/emulated/0/Android/data/com.example.lab7/files/20241109_232854.jpg
     Card(
         modifier = Modifier
@@ -63,8 +41,8 @@ fun ShowImage(name: String, quantity: String, path: String, viewModel: Supermark
                     quantity = quantity, imagePath = path
                 )
                 viewModel.deleteItem(item.imagePath!!)
-            }) {
-
+            },) {
+                Text(text = "Eliminar")
             }
 
             Row(
@@ -118,7 +96,7 @@ fun ShowImage(name: String, quantity: String, path: String, viewModel: Supermark
 
 @Preview(showBackground = true)
 @Composable
-fun ShowImagePreview() {
+fun ItemCardPreview() {
     val applicationContext = LocalContext.current.applicationContext as MyApp
     val supermarketRepository = applicationContext.supermarketRepository
     val supermarketViewModel = SupermarketViewModelFactory(supermarketRepository).create(SupermarketViewModel::class.java)
@@ -127,5 +105,5 @@ fun ShowImagePreview() {
     val quantity = "1"
     val path = "/storage/emulated/0/Android/data/com.example.lab7/files/20241109_232854.jpg"
 
-    ShowImage(name, quantity, path, supermarketViewModel)
+    ItemCard(name, quantity, path, supermarketViewModel)
 }
--- app/src/main/java/com/example/lab7/ui/supermarket/view/SupermarketScreen.kt
+++ app/src/main/java/com/example/lab7/ui/supermarket/view/SupermarketScreen.kt
@@ -1,46 +1,23 @@
 package com.example.lab7.ui.supermarket.view
 
 import android.annotation.SuppressLint
-import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import coil.compose.rememberAsyncImagePainter
-import coil.compose.rememberImagePainter
-import com.example.lab7.MyApp
-import com.example.lab7.R
 import com.example.lab7.navigation.AppBar
 import com.example.lab7.navigation.NavigationState
 import com.example.lab7.navigation.navigateTo
-import com.example.lab7.ui.categories.viewmodel.MealsCategoriesViewModel
-import com.example.lab7.ui.supermarket.repositories.SupermarketRepository
 import com.example.lab7.ui.supermarket.viewmodel.SupermarketViewModel
-import com.example.lab7.ui.supermarket.viewmodel.SupermarketViewModelFactory
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -69,8 +46,8 @@ fun SupermarketScreen(navController: NavController, viewModel: SupermarketViewMo
                 Text(text = "Agregar producto") }
             }
 
-            items(items.value) {
-                it.imagePath?.let { it1 -> ShowImage(it.name, it.quantity, it1, viewModel) }
+            items(items.value) {// Elementos de la lista de productos
+                it.imagePath?.let { it1 -> ItemCard(it.name, it.quantity, it1, viewModel) }
             }
         }
     }
